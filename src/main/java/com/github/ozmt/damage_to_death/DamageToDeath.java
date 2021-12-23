9d96fe8368d40ab66aad0c3d6dfa3ec78cb06633
tomato111 <eidaitanaka10@gmail.com>
2021-12-23 13:55:53 +0900

    メッセージ追加

--- src/main/java/com/github/ozmt/damage_to_death/DamageToDeath.java
+++ src/main/java/com/github/ozmt/damage_to_death/DamageToDeath.java
@@ -7,7 +7,7 @@ public final class DamageToDeath extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getLogger().info("tomato");
     }
 
     @Override
