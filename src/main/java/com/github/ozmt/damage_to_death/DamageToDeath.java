6491d939aab54dd3c3ef5c304e15e60621031cdc
tomato111 <eidaitanaka10@gmail.com>
2021-12-23 13:40:29 +0900

    IntelliJのテンプレ

---
+++ src/main/java/com/github/ozmt/damage_to_death/DamageToDeath.java
@@ -0,0 +1,17 @@
+package com.github.ozmt.damage_to_death;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DamageToDeath extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
