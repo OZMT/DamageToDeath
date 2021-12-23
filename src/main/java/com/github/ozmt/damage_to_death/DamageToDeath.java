a42da04b7b09732bbdb02c54d6af9f184b5585cc
tomato111 <eidaitanaka10@gmail.com>
2021-12-24 01:41:41 +0900

    メッセージ追加

--- src/main/java/com/github/ozmt/damage_to_death/DamageToDeath.java
+++ src/main/java/com/github/ozmt/damage_to_death/DamageToDeath.java
@@ -1,13 +1,18 @@
 package com.github.ozmt.damage_to_death;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Random;
 
 public final class DamageToDeath extends JavaPlugin implements Listener {
 
@@ -35,6 +40,17 @@ public final class DamageToDeath extends JavaPlugin implements Listener {
     @EventHandler
     public void setDeathMessage(PlayerDeathEvent event) {
         event.setDeathMessage("You died");
+    }
+
+    @EventHandler
+    public void CraftingMiss(CraftItemEvent event) {
+        Entity entity = event.getWhoClicked();
+        double random = Math.random();
+        if (random < 0.5D) {
+            event.setCurrentItem(new ItemStack(Material.COAL));
+            entity.sendMessage("クラフトに失敗しました！");
+        }
+
     }
 }
 
